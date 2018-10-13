46e29d96207b9fe56a28f081b5c9ca3b3da00f15
WilliamB <benichou.william@outlook.fr>
2018-10-13 21:20:08 +0200

    Rework on terrain generation and game flow

---
+++ Assets/TemplateManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemplateManager : MonoBehaviour {
+    internal static void OnEnd()
+    {
+        GameManager.Instance.OnTemplateEnd(this);
+    }
+
+    internal void OnStart()
+    {
+        GameManager.Instance.OnTemplateStart(this);
+    }
+}
