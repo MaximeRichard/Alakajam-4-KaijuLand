44dede5d4aee4ad1106181c26a9abbe72715aa35
WilliamB <benichou.william@outlook.fr>
2018-10-13 15:46:57 +0200

    [JAM] Added plateforms inGame Passed to HDRP Added Shadergraph

---
+++ Assets/Scripts/Pooling/ObjectPooling.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooling : MonoBehaviour {
+
+    public int initialPoolSize = 50;
+    public int requestStep = 10;
+    public GameObject poolObjectPrefab;
+
+	List<PoolableObject> pool;
+
+    internal void RequestObject(GameObject gameObject)
+    {
+        
+    }
+    internal void Free(GameObject gameObject)
+    {
+        
+    }
+
+    private void Awake()
+    {
+        for (int i = 0; i < initialPoolSize; i++)
+        {
+            GameObject go = Instantiate(poolObjectPrefab);
+            go.SetActive(false);
+            PoolableObject po = go.GetComponent<PoolableObject>();
+            po.parentPool = this;
+            pool.Add(po);
+        }
+    }
+
+}
