a379b1f24835040ce50616ca033904c53ce5498f
Maxime Richard <maxime.richard06@gmail.com>
2018-10-13 15:10:30 +0200

    Repair incorrect behaviour from changing to 2D colliders

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -7,7 +7,7 @@ public class EnemyBehaviour : MonoBehaviour {
     public Transform leftCheck, rightCheck;
     public LayerMask platformMask;
 
-    private bool isRight;
+    private bool isRight,endOfPlatform;
     private Vector3 movement;
     private Transform currentCheck;
     // Use this for initialization
@@ -23,8 +23,8 @@ public class EnemyBehaviour : MonoBehaviour {
         // Move the object forward along its z axis 1 unit/second.
         transform.Translate(movement * Time.deltaTime * speed);
 
-        //if we are grounded...
-        if (Physics.OverlapSphere(currentCheck.position, edgeCheckRadius, platformMask).Length == 0)
+        endOfPlatform = Physics2D.OverlapCircle(currentCheck.position, edgeCheckRadius, platformMask);
+        if (!endOfPlatform)
         {
             Flip();
         }
